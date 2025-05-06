92e9647f3e49c73fa1688f0cd61285d2a0751b4e
toufeeqsk69 <toufeeqsk69@gmail.com>
2025-05-07 03:09:33 +0530

    rental details update

--- final_project_2/routes/Seller/RentalDetails.js
+++ final_project_2/routes/Seller/RentalDetails.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
 const RentalRequest = require('../../models/RentalRequest');
+const RentalCost = require('../../models/RentalCost');
 
 // Middleware to check seller login
 const isSellerLoggedIn = (req, res, next) => {
@@ -23,15 +24,27 @@ router.get('/rental-details/:id', isSellerLoggedIn, async (req, res) => {
     const rental = await RentalRequest.findOne({
       _id: req.params.id,
       sellerId: req.session.userId
-    });
+    })
+    .populate('buyerId', 'firstName lastName email phone'); // Populate buyerId with specific fields
     
     if (!rental) {
       return res.redirect('/seller_dashboard/view-rentals');
     }
     
+    // Fetch the rental cost to get the total money received
+    const rentalCost = await RentalCost.findOne({ rentalCarId: rental._id });
+    const moneyReceived = rentalCost ? rentalCost.totalCost : null;
+
+    // Format dates for display
+    const formattedPickupDate = rental.pickupDate ? rental.pickupDate.toISOString().split('T')[0] : 'Not specified';
+    const formattedDropDate = rental.dropDate ? rental.dropDate.toISOString().split('T')[0] : 'Not specified';
+
     res.render('seller_dashboard/rental-details', { 
       user,
       rental,
+      formattedPickupDate,
+      formattedDropDate,
+      moneyReceived,
       error: null
     });
   } catch (err) {
@@ -39,7 +52,7 @@ router.get('/rental-details/:id', isSellerLoggedIn, async (req, res) => {
     res.status(500).render('seller_dashboard/rental-details', { 
       user: {},
       rental: null,
-      error: 'Failed to load rental details' 
+      error: 'Failed to load rental details'
     });
   }
 });

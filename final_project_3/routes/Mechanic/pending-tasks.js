64203afd46931511daecf35328eef376b608d9f3
Jeevan116 <jeevangupthav@gmail.com>
2025-05-13 00:13:21 +0530

    tfdt

--- final_project_3/routes/Mechanic/pending-tasks.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-router.get("/pending-tasks", (req, res) => {
-    res.render("mechanic_dashboard/pending-tasks.ejs");
-  });
-
-  module.exports = router;

482457dbf4ec22d637970d11f5f5b13865ab7331
JeevanVankadara <jeevangupthav@gmail.com>
2025-11-15 12:29:54 +0530

    updated App.jsx by adding buyer related things

--- React_DriveBidRent/Frontend/src/App.jsx
+++ React_DriveBidRent/Frontend/src/App.jsx
@@ -5,32 +5,40 @@ import HomePage from './pages/auth/HomePage';
 import Login from './pages/auth/Login';
 import Signup from './pages/auth/Signup';
 
+// Buyer
 import BuyerLayout from './pages/buyer/BuyerLayout';
 import BuyerDashboard from './pages/buyer/Dashboard';
 
+// Seller
+import SellerLayout from './pages/seller/SellerLayout';
+import SellerDashboard from './pages/seller/Dashboard';
+
 function App() {
     return (
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-        <Route path="/buyer" element={<BuyerLayout />}>
-            <Route index element={<BuyerDashboard />} />
-            <Route path="dashboard" element={<BuyerDashboard />} />
-        </Route>
+            <Route path="/buyer" element={<BuyerLayout />}>
+                <Route index element={<BuyerDashboard />} />
+                <Route path="dashboard" element={<BuyerDashboard />} />
+            </Route>
 
-        <Route
-            path="*"
-            element={
-                <div className="flex items-center justify-center min-h-screen text-2xl font-bold text-gray-700">
-                    404 - Page Not Found
-                </div>
-            }
-        />
-    </Routes>
+            <Route path="/seller" element={<SellerLayout />}>
+                <Route index element={<SellerDashboard />} />
+                <Route path="dashboard" element={<SellerDashboard />} />
+            </Route>
 
-
+            <Route
+                path="*"
+                element={
+                    <div className="flex items-center justify-center min-h-screen text-2xl font-bold text-gray-700">
+                        404 - Page Not Found
+                    </div>
+                }
+            />
+        </Routes>
     );
 }
 
